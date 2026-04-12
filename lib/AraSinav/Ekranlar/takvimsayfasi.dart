d2b6d4a83f683996d39d6a9ff27742e5fcff4d26
hakangencoglu <hakan.gencoglu@izu.edu.tr>
2026-04-12 03:54:45 +0000

    Anlık eklenen örnekler silindi

--- lib/AraSinav/Ekranlar/takvimsayfasi.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-
-class TakvimSayfasi extends StatelessWidget {
-  const TakvimSayfasi({super.key});
-  @override
-  Widget build(BuildContext context) =>
-      const Center(child: Text("Akademik Takvim"));
-}
